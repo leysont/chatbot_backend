ca7e91e0f51ebb20d17cd729263dac0fffbcc4e6
leysont <leysont@tutanota.com>
2024-10-02 13:31:38 +0200

    feat(Models): replace issueDescription with issueExperience and issueExpectation

--- storage/src/main/kotlin/models/Ticket.kt
+++ storage/src/main/kotlin/models/Ticket.kt
@@ -4,24 +4,28 @@ import data.ServiceLevel
 import kotlinx.serialization.Serializable
 
 /**
- * Represents a ticket in the system.
+ * Represents a support ticket in the system.
  *
- * @property id The unique identifier for the ticket.
- * @property title The title of the ticket.
- * @property customer The customer who reported the issue.
- * @property issueDescription A detailed description of the issue.
- * @property employee The employee assigned to handle the ticket.
- * @property express Indicates whether the ticket is marked as express.
- * @property serviceLevel The service level of the ticket, defaults to the customer's service level.
+ * @property id The unique identifier for the ticket. This value is nullable and defaults to null.
+ * @property title A brief title summarizing the issue of the ticket.
+ * @property customer The customer who reported the issue. This is an instance of the Customer class.
+ * @property issueExperience A detailed description of what the customer experienced.
+ * @property issueExpectation A detailed description of what the customer expected instead of the issue.
+ * @property employee The employee assigned to handle the ticket. This is an instance of the Employee class.
+ * @property express Indicates whether the ticket is marked as express (requires urgent attention). Defaults to false.
+ * @property tags A list of tags associated with the ticket. Defaults to an empty list.
+ * @property serviceLevel The service level of the ticket, inherited from the customer's service level.
  */
 @Serializable
 data class Ticket(
     val id: Int? = null,
     val title: String,
     val customer: Customer,
-    val issueDescription: String,
+    val issueExperience: String,
+    val issueExpectation: String,
     val employee: Employee,
     val express: Boolean = false,
+    val tags: List<String> = listOf(),
 ) {
     val serviceLevel: ServiceLevel = customer.serviceLevel
 }
